122cfa5f41f22a1dab124c38b97e6d88c6a8e48e
Juancho <linanjm90@gmail.com>
2017-07-27 21:01:59 +0100

    call the method to show an error when the email is empty

--- app/src/main/java/mx/juanma/multitask/modules/Login/LoginPresenter.kt
+++ app/src/main/java/mx/juanma/multitask/modules/Login/LoginPresenter.kt
@@ -9,8 +9,18 @@ package mx.juanma.multitask.modules.Login
 class LoginPresenter(val mView: ILoginView, val mInteractor: ILoginInteractor) {
 
     fun onLoginBtnClick() {
+        val email = mView.getEmail()
+        if(!validateEmail(email)) return
     }
 
     fun signUp(email: String, password: String, capture: ILoginInteractor.Callback) {}
+
+    private fun validateEmail(email: String?): Boolean {
+        if(email == null || email.isEmpty()) {
+            this.mView.showEmailRequiredError()
+            return false
+        }
+        return true
+    }
 
 }
