a2b5a68120f47c5b3c52dadd65c8187930419141
Juancho <linanjm90@gmail.com>
2017-07-27 19:18:26 +0100

    add the test file for the login presenter file

---
+++ app/src/test/java/mx/juanma/multitask/modules/Login/LoginPresenterTest.kt
@@ -0,0 +1,19 @@
+package mx.juanma.multitask.modules.Login
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+/**
+ * Created by Juancho on 27/07/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+class LoginPresenterTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+}
