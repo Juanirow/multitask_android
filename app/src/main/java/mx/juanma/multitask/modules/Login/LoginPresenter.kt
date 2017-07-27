e31f35f40f50dc564f5a4b034916fc0a95d5c446
Juancho <linanjm90@gmail.com>
2017-07-27 21:25:52 +0100

    show error message when the password is empty and the user try to login

--- app/src/main/java/mx/juanma/multitask/modules/Login/LoginPresenter.kt
+++ app/src/main/java/mx/juanma/multitask/modules/Login/LoginPresenter.kt
@@ -13,9 +13,21 @@ class LoginPresenter(val mView: ILoginView, val mInteractor: ILoginInteractor) {
     fun onLoginBtnClick() {
         val email = mView.getEmail()
         if(!validateEmail(email)) return
+        val password = mView.getPassword()
+        if(!validatePassword(password)) return
+
     }
 
     fun signUp(email: String, password: String, capture: ILoginInteractor.Callback) {}
+
+    private fun validatePassword(password: String?): Boolean {
+        if(password == null || password.isEmpty()) {
+            this.mView.showPasswordRequiredError()
+            return false
+        }
+
+        return true
+    }
 
     private fun validateEmail(email: String?): Boolean {
         if(email == null || email.isEmpty()) {
