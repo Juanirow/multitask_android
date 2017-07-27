bca95bc9c39480e82ff60839667a2614b6450fca
Juancho <linanjm90@gmail.com>
2017-07-27 22:28:41 +0100

    add the activity for the login

---
+++ app/src/main/java/mx/juanma/multitask/modules/Login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package mx.juanma.multitask.modules.Login
+
+import android.app.Activity
+import android.os.Bundle
+
+import mx.juanma.multitask.R
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
