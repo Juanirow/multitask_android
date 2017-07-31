b98c3735bfa691725a5e07495b721a574bdbf32e
Juancho <linanjm90@gmail.com>
2017-07-31 21:12:17 +0100

    create the contracts and the presenter for the create account module

---
+++ app/src/main/java/mx/juanma/multitask/modules/CreateAccount/CreateAccountPresenter.kt
@@ -0,0 +1,10 @@
+package mx.juanma.multitask.modules.CreateAccount
+
+
+/**
+ * Created by Juancho on 31/07/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+class CreateAccountPresenter {
+}
---
+++ app/src/main/java/mx/juanma/multitask/modules/CreateAccount/ICreateAccountInteractor.kt
@@ -0,0 +1,14 @@
+package mx.juanma.multitask.modules.CreateAccount
+
+
+/**
+ * Created by Juancho on 31/07/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+interface ICreateAccountInteractor {
+
+    interface Callback {
+
+    }
+}
