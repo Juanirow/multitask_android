6bb605f51d017956a8f4379801de675991c74db8
Juancho <linanjm90@gmail.com>
2017-07-31 23:02:03 +0100

    add the activity layout and class for the create account module

---
+++ app/src/main/java/mx/juanma/multitask/modules/CreateAccount/CreateAccountActivity.kt
@@ -0,0 +1,14 @@
+package mx.juanma.multitask.modules.CreateAccount
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import mx.juanma.multitask.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
