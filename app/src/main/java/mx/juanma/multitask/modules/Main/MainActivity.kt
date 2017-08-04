501806c62e741faa08a84f283b044a8d2280e211
Juancho <linanjm90@gmail.com>
2017-08-04 18:09:42 +0100

    refactor the menu options in the nav drawer

--- app/src/main/java/mx/juanma/multitask/modules/Main/MainActivity.kt
+++ app/src/main/java/mx/juanma/multitask/modules/Main/MainActivity.kt
@@ -72,19 +72,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         val id = item.itemId
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        when(id) {
+//            R.id.nav_logout -> { }
+//        }
 
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
         drawer.closeDrawer(GravityCompat.START)
