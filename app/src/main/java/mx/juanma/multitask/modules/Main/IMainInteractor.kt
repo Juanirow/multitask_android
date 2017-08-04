6002825ebac37b8d49561973fd3d25d1170d890e
Juancho <linanjm90@gmail.com>
2017-08-04 18:23:24 +0100

    add the test for the main activity

---
+++ app/src/main/java/mx/juanma/multitask/modules/Main/IMainInteractor.kt
@@ -0,0 +1,11 @@
+package mx.juanma.multitask.modules.Main
+
+
+/**
+ * Created by Juancho on 04/08/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+interface IMainInteractor {
+    fun logout()
+}
---
+++ app/src/test/java/mx/juanma/multitask/modules/Main/MainPresenterTest.kt
@@ -0,0 +1,43 @@
+package mx.juanma.multitask.modules.Main
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+/**
+ * Created by Juancho on 04/08/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+class MainPresenterTest {
+
+    @Mock
+    lateinit var mView: IMainView
+
+    @Mock
+    lateinit var mInteractor: IMainInteractor
+
+    lateinit var mPresenter: MainPresenter
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        mPresenter = MainPresenter(mView, mInteractor)
+    }
+
+    @After
+    fun tearDown() {
+        Mockito.verifyNoMoreInteractions(mView, mInteractor)
+    }
+
+    @Test
+    fun shouldCloseActivityOkResult() {
+        mPresenter.onClickLogout()
+
+        Mockito.verify(this.mInteractor).logout()
+        Mockito.verify(this.mView).finishActivityWithOkResult()
+    }
+}
