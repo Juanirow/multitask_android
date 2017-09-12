99a3b280e812a86f5ccf2578f5f9a88761ba5007
Juancho <linanjm90@gmail.com>
2017-09-12 23:11:42 +0100

    add the tool bar to the activity in the add category activity

--- app/src/main/java/mx/juanma/multitask/modules/AddCategory/AddCategoryActivity.kt
+++ app/src/main/java/mx/juanma/multitask/modules/AddCategory/AddCategoryActivity.kt
@@ -2,6 +2,7 @@ package mx.juanma.multitask.modules.AddCategory
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.app_bar_container.*
 
 import mx.juanma.multitask.R
 
@@ -11,9 +12,18 @@ class AddCategoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_category)
 
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         this.fragmentManager.beginTransaction()
                 .add(R.id.content_add_category, AddCategoryFragment.getInstance())
                 .commit()
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 }
