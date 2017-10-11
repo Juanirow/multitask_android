26a70e95278c2ea5f782a20da2ae82ff8e1796fe
Juancho <linanjm90@gmail.com>
2017-10-11 20:45:01 +0100

    new fragment for the module

---
+++ app/src/main/java/mx/juanma/multitask/modules/EditCategory/EditCategoryFragment.kt
@@ -0,0 +1,36 @@
+package mx.juanma.multitask.modules.EditCategory
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mx.juanma.multitask.R
+
+
+/**
+ * Created by Juancho on 11/10/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+class EditCategoryFragment: Fragment() {
+    
+    companion object {
+        @JvmStatic fun getInstance(): EditCategoryFragment {
+            val fragment = EditCategoryFragment()
+            fragment.retainInstance = true
+            return fragment
+         }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(R.layout.fragment_edit_category, container, false)
+        return view!!
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        //TODO implement Presenter
+        super.onActivityCreated(savedInstanceState)
+    }
+}
