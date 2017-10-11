ca4c0eadd25981d81824a91069efdfe7fd992ab9
Juancho <linanjm90@gmail.com>
2017-10-11 16:46:08 +0100

    add the module files to edit a category

---
+++ app/src/main/java/mx/juanma/multitask/modules/EditCategory/IEditCategoryInteractor.kt
@@ -0,0 +1,14 @@
+package mx.juanma.multitask.modules.EditCategory
+
+
+/**
+ * Created by Juancho on 11/10/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+interface IEditCategoryInteractor {
+
+    public interface Callback {
+
+    }
+}
---
+++ app/src/main/java/mx/juanma/multitask/modules/EditCategory/IEditCategoryView.kt
@@ -0,0 +1,10 @@
+package mx.juanma.multitask.modules.EditCategory
+
+
+/**
+ * Created by Juancho on 11/10/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+interface IEditCategoryView {
+}
